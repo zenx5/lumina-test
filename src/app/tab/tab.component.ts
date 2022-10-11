6a7591778685a2dabad2d98e5ad3b634dde18cea
moises <elmoises.reydereymail.com>
2022-10-10 21:14:36 -0400

    agg components a tab

--- src/app/tab/tab.component.ts
+++ src/app/tab/tab.component.ts
@@ -10,16 +10,13 @@ export class TabComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // sessionStorage.getItem("tabMenu") == null ? sessionStorage.setItem('tabMenu', 'dashboard') :  console.log('error');
 
         let tabLi = document.querySelectorAll(".tabMenu li");
         let tabs = document.querySelectorAll(".tabMenu li span");
 
         tabs.forEach( tab => {  
                         
-            tab.addEventListener('click', function(event) {
-
-                // sessionStorage.setItem('tabMenu', tab.className);              
+            tab.addEventListener('click', function(event) {        
 
                 // not active tabs at the moment
                 tabLi.forEach(t => {                       
@@ -31,7 +28,6 @@ export class TabComponent implements OnInit {
 
                     // select the second class from tabContent
                     let className = element ? element.className.split(" ") : null;
-                    console.log(className)
                     // hide or show tabContent
                     element.classList.contains( tab.className ) ? tab.parentElement?.setAttribute('class', 'tabLi active') : null;
                     element.classList.contains( tab.className ) ? element.classList.add('active') : element.classList.remove('active');
